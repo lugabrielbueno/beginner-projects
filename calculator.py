85e454ec25a3e95d5c5f7c8283e046bde363666a
Lucas Gabriel Bueno <gabriel-euro@hotmail.com.br>
2020-06-01 12:35:55 -0300

    layout ajusts

--- calculator.py
+++ calculator.py
@@ -10,41 +10,41 @@ def plus():
 
 
 Janela_inicial = Tk()
-Janela_inicial.title('Sistema')
+Janela_inicial.title('Calculator')
 Janela_inicial['bg'] = 'grey'
-Janela_inicial.geometry('500x300+350+250')
-Janela_inicial.minsize(500,300)
+Janela_inicial.geometry('220x350+350+250')
+Janela_inicial.minsize(220,350)
+Janela_inicial.maxsize(220,350)
 answer = StringVar()
 
 
 # WIDGETS
 
-inputs = Entry(Janela_inicial)
+inputs = Entry(Janela_inicial,width = 25,borderwidth = 5)
 inputs.focus()
 
 
-label_result = Label(Janela_inicial,textvariable= answer)
-bt1 = Button(Janela_inicial,text = '1',command=1)
-bt2 = Button(Janela_inicial,text = '2',command=2)
-bt3 = Button(Janela_inicial,text = '3',command=3)
-bt4 = Button(Janela_inicial,text = '4',command=4)
-bt5 = Button(Janela_inicial,text = '5',command=5)
-bt6 = Button(Janela_inicial,text = '6',command=6)
-bt7 = Button(Janela_inicial,text = '7',command=7)
-bt8 = Button(Janela_inicial,text = '8',command=8)
-bt9 = Button(Janela_inicial,text = '9',command=9)
-bt0 = Button(Janela_inicial,text = '0',command=0)
-btdot = Button(Janela_inicial,text = '.',command=1)
-btbar = Button(Janela_inicial,text = '/',command = '/')
-btplus = Button(Janela_inicial,text = '+',command=plus)
-btsub = Button(Janela_inicial,text = '-',command='-')
-btequal = Button(Janela_inicial,text = '=',command=equal)
-btmult = Button(Janela_inicial,text = '*',command= '*')
-
+bt1 = Button(Janela_inicial,text = '1',command=1,padx=20 ,pady=20)
+bt2 = Button(Janela_inicial,text = '2',command=2,padx=20 ,pady=20)
+bt3 = Button(Janela_inicial,text = '3',command=3,padx=20 ,pady=20)
+bt4 = Button(Janela_inicial,text = '4',command=4,padx=20 ,pady=20)
+bt5 = Button(Janela_inicial,text = '5',command=5,padx=20 ,pady=20)
+bt6 = Button(Janela_inicial,text = '6',command=6,padx=20 ,pady=20)
+bt7 = Button(Janela_inicial,text = '7',command=7,padx=20 ,pady=20)
+bt8 = Button(Janela_inicial,text = '8',command=8,padx=20 ,pady=20)
+bt9 = Button(Janela_inicial,text = '9',command=9,padx=20 ,pady=20)
+bt0 = Button(Janela_inicial,text = '0',command=0,padx=20 ,pady=20)
+btdot = Button(Janela_inicial,text = '.',command=1,padx=20 ,pady=20)
+btbar = Button(Janela_inicial,text = '/',command = '/',padx=20 ,pady=20)
+btplus = Button(Janela_inicial,text = '+',command=plus,padx=20 ,pady=20)
+btsub = Button(Janela_inicial,text = '-',command='-',padx=20 ,pady=20)
+btequal = Button(Janela_inicial,text = '=',command=equal,padx=20 ,pady=20)
+btmult = Button(Janela_inicial,text = '*',command= '*',padx=20 ,pady=20)
+btclear = Button(Janela_inicial,text = 'Clear',command = 'C',padx = 88, pady = 20)
 
 
 #LAYOUT
-inputs.grid(row = 0, column = 0)
+inputs.grid(row = 0, column = 0,columnspan = 4)
 bt1.grid(row = 1 ,column = 0)
 bt2.grid(row = 1 ,column = 1)
 bt3.grid(row = 1 ,column = 2)
@@ -61,6 +61,6 @@ bt0.grid(row = 4 ,column = 0)
 btdot.grid(row = 4 ,column = 1)
 btbar.grid(row = 4,column =2)
 btequal.grid(row=4,column=3)
-label_result.grid(row = 5 ,column = 0)
+btclear.grid(row =5,column = 0 ,  columnspan = 4)
 
 Janela_inicial.mainloop()
